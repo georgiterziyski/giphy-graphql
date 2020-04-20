d1b85ec19c446b4eea90a8ae04972dfaf63133ed
georgiterziyski <56469632+georgiterziyski@users.noreply.github.com>
2020-04-20 19:27:58 +0300

    srever.js testing

--- src/server.js
+++ src/server.js
@@ -13,7 +13,11 @@ const app = express();
 const PORT = process.env.PORT || "3000";
 const db = "mongodb+srv://terzy:1q2w3e4r@cluster0-lmi5s.mongodb.net/Test?retryWrites=true&w=majority";//process.env.MONGODB_URL;
 
-mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(()=>{
+mongoose.connect(db, {
+                     useNewUrlParser: true,
+                     //useUnifiedTopology: true,
+                     useFindAndModify: false
+                }).then(()=>{
     console.log("Connected to MongoDB");
 }).catch(error => console.log(error));
 
